df5173f410cfee486089c96797f657061837b96e
leonardo <leopereirasilva86@gmail.com>
2023-12-29 01:39:30 -0300

    win condition

--- src/App.jsx
+++ src/App.jsx
@@ -34,24 +34,26 @@ function App() {
   const [score, setScore] = useState(0);
 
   
-  const pickWordAndCategory = () => {
+  const pickWordAndCategory = useCallback(() => {
     //pick a random category
     const categories = Object.keys(words)
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
 
-    console.log(category);
 
     //pick a random word
     const word = words[category][Math.floor(Math.random() * words[category].length)]
 
-    console.log(word);
+
 
     return {word, category}
-  }
+  }, [words])
 
 
   // starts the secret word game
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    //clear all letters
+    clearLetterStates();
+
     //pick word and pick category
     const {word, category} = pickWordAndCategory();
 
@@ -61,16 +63,13 @@ function App() {
 
     wordLetters = wordLetters.map((l) => l.toLowerCase());
 
-    console.log(word, category)
-    console.log(wordLetters);
-
     //fill states
     setPickedWord(word);
     setPickedCategory(category);
     setLetters(wordLetters);
 
     setGameStage(stages[1].name);
-  }
+  }, [pickWordAndCategory]);
 
   // process the letter input
   const verifyLetter = (letter) => {
@@ -102,6 +101,7 @@ function App() {
     setWrongLetters([]);
   }
 
+  // check if guesses ended
   useEffect(() => {
     if(guesses <= 0){
       // reset all states
@@ -110,6 +110,21 @@ function App() {
       setGameStage(stages[2].name);
     }
   }, [guesses])
+
+  //check win condition
+  useEffect(() => {
+    const uniqueLetters = [... new Set(letters)];
+
+    //win condition
+    if(guessedLetters.length === uniqueLetters.length){
+      // add score
+      setScore((actualScore) => actualScore += 100)
+
+      // restart game with new word
+      startGame();
+    }
+
+  }, [guessedLetters, letters, startGame])
 
   //retry game
   const retry = () => {
